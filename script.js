9511b06f302db581b2b60d20def1fa576bca0e98
Efe <efegun823@gmail.com>
2022-09-17 13:41:35 +0200

    contact

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Logo Link
+
+const logo = document.getElementsByClassName('logo');
+
+function logoLink() {
+  window.location.href = 'index.html';
+}
+
+// Scroll Down Animation
+
 function ScrollDown() {
   window.scrollBy(0, 780);
 }
